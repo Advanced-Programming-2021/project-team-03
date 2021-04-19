ba3f7c63c5976707006790ab92d38dc9fb72c006
MMVahedi <m.m.vahedi13800@gmail.com>
2021-04-19 13:25:23 +0430

    updating board class: add card position in field class. add constructor.

--- src/main/java/model/game/Board.java
+++ src/main/java/model/game/Board.java
@@ -7,50 +7,100 @@ import model.card.Monster;
 import model.card.SpellAndTrap;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
-import static model.game.attackingPosition.ATTACKING_POSITION;
+enum AttackingOrDefending {
+    ATTACKING,
+    DEFENDING
+}
 
-enum attackingPosition{
-    ATTACKING_POSITION(0),
-    DEFENDING_POSITION(1);
+enum FaceUpOrFaceDown {
+    FACE_UP,
+    FACE_DOWN,
+}
 
-    private final int value;
+class CardPositionInField {
+    public AttackingOrDefending attackingOrDefending;
+    public FaceUpOrFaceDown faceUpOrFaceDown;
+    public int position;
 
-    attackingPosition(int value) {
-    this.value = value;
+    public CardPositionInField(int position, FaceUpOrFaceDown faceUpOrFaceDown, AttackingOrDefending attackingOrDefending){
+        this.position = position;
+        this.faceUpOrFaceDown = faceUpOrFaceDown;
+        this.attackingOrDefending = attackingOrDefending;
     }
 
-    public int getValue() {
-        return value;
+    public AttackingOrDefending getAttackingOrDefending() {
+        return attackingOrDefending;
+    }
+
+    public void setAttackingOrDefending(AttackingOrDefending attackingOrDefending) {
+        this.attackingOrDefending = attackingOrDefending;
+    }
+
+    public FaceUpOrFaceDown getFaceUpOrFaceDown() {
+        return faceUpOrFaceDown;
+    }
+
+    public void setFaceUpOrFaceDown(FaceUpOrFaceDown faceUpOrFaceDown) {
+        this.faceUpOrFaceDown = faceUpOrFaceDown;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
     }
 }
 
 public class Board {
     private User owner;
-    private HashMap<Monster, Integer[]> monstersInField; // holding each monster with its position and revealed status
-    private HashMap<SpellAndTrap, Integer[]> spellAndTrapsInField; // holding all spells and traps in the field
+    private HashMap<CardPositionInField, Monster> monstersInField; // holding each monster with its position and revealed status
+    private HashMap<CardPositionInField, SpellAndTrap> spellAndTrapsInField; // holding all spells and traps in the field
     private ArrayList<Card> graveyard;
-    private Card fieldCard;
+    private SpellAndTrap fieldCard;
     private ArrayList<Card> remainingCards;
     private ArrayList<Card> inHandCards;
 
     public Board(Deck deck, User player) {
-            monstersInField = new HashMap<>();
-            /*Integer[] newInt = new Integer[]{0,2,ATTACKING_POSITION.getValue()};
+        this.owner = player;
+        monstersInField = new HashMap<>();
+        spellAndTrapsInField = new HashMap<>();
+        graveyard = new ArrayList<>();
+        remainingCards = new ArrayList<>();
+        addStartingDeckToTheRemainingCards(deck); // add main deck cards to the remaining cards Arraylist
+        inHandCards = new ArrayList<>();
+        addStartingCardsToInHandCards(); // adding 4 card from top of remaining cards to hand to start the game
+    }
 
-            monstersInField.put(new Monster(),);*/
+    private void addStartingDeckToTheRemainingCards(Deck deck){
+        remainingCards.addAll(deck.getMainDeck());
+        Collections.shuffle(remainingCards); // shuffle all cards for start the game
+    }
+
+    public void addCardFromRemainingToInHandCards(){
+        inHandCards.add(remainingCards.get(0));
+        remainingCards.remove(0);
+    }
+
+    private void addStartingCardsToInHandCards(){
+        for (int i = 0; i < 4; i++) {
+            addCardFromRemainingToInHandCards();
+        }
     }
 
     public User getOwner() {
         return owner;
     }
 
-    public HashMap<Monster, Integer[]> getMonstersInField() {
+    public HashMap<CardPositionInField, Monster> getMonstersInField() {
         return monstersInField;
     }
 
-    public HashMap<SpellAndTrap, Integer[]> getSpellAndTrapsInField() {
+    public HashMap<CardPositionInField, SpellAndTrap> getSpellAndTrapsInField() {
         return spellAndTrapsInField;
     }
 
@@ -67,7 +117,7 @@ public class Board {
         return fieldCard;
     }
 
-    public void setFieldCard(Card fieldCard) {
+    public void setFieldCard(SpellAndTrap fieldCard) {
         this.fieldCard = fieldCard;
     }
 
