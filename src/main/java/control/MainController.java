9acf55aa61e5329e25c2ade2d6145a251b8d2214
Ali Razghandi <a.razghandi11@gmail.com>
2021-04-24 00:40:55 +0430

    updating the mindrot library for password hash function

--- src/main/java/control/MainController.java
+++ src/main/java/control/MainController.java
@@ -1,5 +1,8 @@
 package control;
 
+import org.json.JSONObject;
+
+
 public class MainController { // this class is responsible for view request and send the feedback to thee view via a Json string
 
     private static MainController mainControllerInstance;
@@ -15,8 +18,10 @@ public class MainController { // this class is responsible for view request and 
 
     public String getRequest(String input){ //this method receives a input string and return a string as an answer
         /* note that this strings are in Json format */
-
         // TODO parsing analysing and answering the request of view menu
+        JSONObject inputObject = new JSONObject(input);
+        String requestType = inputObject.getString("requestType");
+
         return null;
     }
 }
