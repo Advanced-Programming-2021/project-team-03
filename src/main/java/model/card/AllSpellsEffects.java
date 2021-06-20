8e9e8291049ff76cbc69d62464217812bffb8e61
Ali Razghandi <a.razghandi11@gmail.com>
2021-06-20 21:56:26 +0430

    add Raigeki effect

--- src/main/java/model/card/AllSpellsEffects.java
+++ src/main/java/model/card/AllSpellsEffects.java
@@ -1,8 +1,12 @@
 package model.card;
 
 import control.game.Update;
+import model.game.Board;
 import model.game.Game;
 import model.game.PlayerTurn;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class AllSpellsEffects {
     private static AllSpellsEffects allSpellsEffects;
@@ -38,7 +42,13 @@ public class AllSpellsEffects {
     }
 
     private void raigekiEffect(Game game, Update gameUpdates, PlayerTurn turn) {
-
+        Board opponentBoard = game.getPlayerOpponentByTurn(turn).getBoard();
+        ArrayList<Monster> allOpponentsMonsters = new ArrayList<>(opponentBoard.getMonstersInField().values());
+        for (Monster opponentMonster : allOpponentsMonsters) {
+            opponentBoard.removeCardFromField(opponentBoard.getMonsterPosition(opponentMonster), true);
+            opponentBoard.addCardToGraveyard(opponentMonster);
+            gameUpdates.addMonsterToGraveyard(opponentMonster);
+        }
     }
 
     private void potOfGreedEffect(Game game, PlayerTurn turn) {
