f8f841bcb0d8e79fb22640beb1ed721e9074d2de
MMVahedi <m.m.vahedi13800@gmail.com>
2021-06-25 13:45:15 +0430

    add name value to TrapNames enum.

--- src/main/java/model/enums/TrapNames.java
+++ src/main/java/model/enums/TrapNames.java
@@ -1,14 +1,24 @@
 package model.enums;
 
 public enum TrapNames {
-    MAGIC_CYLINDER,
-    MIRROR_FORCE,
-    MIND_CRUSH,
-    TRAP_HOLE,
-    TORRENTIAL_TRIBUTE,
-    TIME_SEAL,
-    NEGATE_ATTACK,
-    SOLEMN_WARNING,
-    MAGIC_JAMMER,
-    CALL_OF_THE_HAUNTED;
+    MAGIC_CYLINDER("Magic Cylinder"),
+    MIRROR_FORCE("Mirror Force"),
+    MIND_CRUSH("Mind Crush"),
+    TRAP_HOLE("Trap Hole"),
+    TORRENTIAL_TRIBUTE("Torrential Tribute"),
+    TIME_SEAL("Time Seal"),
+    NEGATE_ATTACK("Negate Attack"),
+    SOLEMN_WARNING("Solemn Warning"),
+    MAGIC_JAMAMER("Magic Jamamer"),
+    CALL_OF_THE_HAUNTED("Call of The Haunted");
+
+    private String name;
+
+    TrapNames(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
