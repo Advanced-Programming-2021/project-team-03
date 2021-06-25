b03d6bb2831243e2701ca2b1971393f003a1d04f
Ali Razghandi <a.razghandi11@gmail.com>
2021-06-25 21:08:39 +0430

    AI initialized

--- src/main/java/control/game/AIController.java
+++ src/main/java/control/game/AIController.java
@@ -1,5 +1,8 @@
 package control.game;
 
+import control.MainController;
+import model.card.Card;
+import model.card.Monster;
 import model.game.Game;
 import model.game.Player;
 
@@ -29,6 +32,80 @@ public class AIController {
     }
 
     public void play() {
+        StringBuilder AIPlayLog = new StringBuilder();
+        AIPlayLog.append("Now it's AI turn\n");
+        bot.getBoard().addCardFromRemainingToInHandCards();
+        AIPlayLog.append("AI Draws a card\n");
+        if (canSummonOrSet()) {
+            boolean summon = summonOrSet();
+            if (summon)
+                AIPlayLog.append("AI Summons a monster in attacking position\n");
+            else
+                AIPlayLog.append("AI Set a monster in defending position\n");
+        }
+
+        while (canSetSpellsInHand()) {
+            setSpell();
+            AIPlayLog.append("AI Set a spell in field\n");
+        }
+
+        while (canAttack()) {
+            attack();
+            AIPlayLog.append("AI attacked!!!\n");
+        }
+        MainController.getInstance().sendPrintRequestToView(AIPlayLog.toString());
+    }
+
+    private void attack() {
 
     }
+
+    private boolean canAttack() {
+
+        return false;
+    }
+
+    private void setSpell() {
+
+    }
+
+    private boolean canSetSpellsInHand() {
+
+        return false;
+    }
+
+    private boolean summonOrSet() {
+
+        return false;
+    }
+
+    private void set() {
+
+    }
+
+    private void summon() {
+
+    }
+
+    private boolean canSummonOrSet() {
+        for (Card card : bot.getBoard().getInHandCards()) {
+            if (card instanceof Monster) {
+                Monster monster = (Monster) card;
+                if (monster.getLevel() < 5)
+                    return true;
+                else return checkForTribute(monster.getLevel());
+            }
+        }
+        return false;
+    }
+
+    private boolean checkForTribute(int level) {
+        if (level < 7) {
+            return bot.getBoard().getMonstersInField().size() > 0;
+        } else {
+            return bot.getBoard().getMonstersInField().size() > 1;
+        }
+    }
+
+
 }
