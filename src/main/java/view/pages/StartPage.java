fd857096dfa4ef0a12f414823db13b68df9e9f6e
Ali Razghandi <a.razghandi11@gmail.com>
2021-07-02 18:43:28 +0430

    add starting page

--- src/main/java/view/pages/StartPage.java
+++ src/main/java/view/pages/StartPage.java
@@ -1,4 +1,19 @@
 package view.pages;
 
-public class StartPage {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class StartPage extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent startingPane = FXMLLoader.load(getClass().getResource("/view/fxml/Start.fxml"));
+        stage.setScene(new Scene(startingPane));
+        stage.getIcons().add(new Image(String.valueOf(getClass().getResource("/assets/pageimages/logo.png"))));
+        stage.setTitle("YU-GI-OH");
+        stage.show();
+    }
 }
