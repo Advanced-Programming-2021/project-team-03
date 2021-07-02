1ff547fae2d0e5920f3fd7bae603c5e8e6195e8b
MMVahedi <m.m.vahedi13800@gmail.com>
2021-07-02 16:08:35 +0430

    MainView: add register method to MainView.

--- src/main/java/view/viewcontroller/MainView.java
+++ src/main/java/view/viewcontroller/MainView.java
@@ -1,4 +1,32 @@
 package view.viewcontroller;
 
+import control.MainController;
+import org.json.JSONObject;
+
 public class MainView {
+    private static MainView instance;
+
+    private MainView() {
+    }
+
+    public static MainView getInstance() {
+        if (instance == null) instance = new MainView();
+        return instance;
+    }
+
+    private JSONObject sendRequestToControl(JSONObject messageToSend) {
+        String controlAnswerString = MainController.getInstance().getRequest(messageToSend.toString());
+        return new JSONObject(controlAnswerString);
+    }
+
+    public JSONObject register(String username, String password, String nickname) {
+        JSONObject value = new JSONObject();
+        value.put("Username", username);
+        value.put("Password", password);
+        value.put("Nickname", nickname);
+        JSONObject messageToSendToControl = new JSONObject();
+        messageToSendToControl.put("Type", "Register");
+        messageToSendToControl.put("Value", value);
+        return sendRequestToControl(messageToSendToControl);
+    }
 }
