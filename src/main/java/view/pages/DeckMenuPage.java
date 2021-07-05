d24601ea868328491dfbee126f6d4511a24f13a0
MMVahedi <m.m.vahedi13800@gmail.com>
2021-07-05 15:04:11 +0430

    add start methods of page classes and add pages fxml files.

--- src/main/java/view/pages/DeckMenuPage.java
+++ src/main/java/view/pages/DeckMenuPage.java
@@ -1,11 +1,18 @@
 package view.pages;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class DeckMenuPage extends Application {
-    @Override
-    public void start(Stage stage) throws Exception {
+    private static Stage stage;
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent startingPane = FXMLLoader.load(getClass().getResource("/view/fxml/DeckMenu.fxml"));
+        primaryStage.setScene(new Scene(startingPane));
+        stage = primaryStage;
     }
 }
--- src/main/java/view/pages/ShopMenuPage.java
+++ src/main/java/view/pages/ShopMenuPage.java
@@ -1,11 +1,18 @@
 package view.pages;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ShopMenuPage extends Application {
-    @Override
-    public void start(Stage stage) throws Exception {
+    private static Stage stage;
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent startingPane = FXMLLoader.load(getClass().getResource("/view/fxml/ShopMenu.fxml"));
+        primaryStage.setScene(new Scene(startingPane));
+        stage = primaryStage;
     }
 }
