2b38900f8d32c4b51bc17324491670de82d8ba30
MMVahedi <m.m.vahedi13800@gmail.com>
2021-07-05 12:38:20 +0430

    complete RegisterPage control

--- src/main/java/view/pages/RegisterPage.java
+++ src/main/java/view/pages/RegisterPage.java
@@ -7,8 +7,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import org.json.JSONObject;
+import view.viewcontroller.MainView;
 
 public class RegisterPage extends Application {
     private static Stage stage;
@@ -22,6 +25,7 @@ public class RegisterPage extends Application {
         Parent startingPane = FXMLLoader.load(getClass().getResource("/view/fxml/Register.fxml"));
         primaryStage.setScene(new Scene(startingPane));
         stage = primaryStage;
+        stage.show();
     }
 
     public void back(MouseEvent mouseEvent) throws Exception {
@@ -29,5 +33,29 @@ public class RegisterPage extends Application {
     }
 
     public void login(MouseEvent mouseEvent) {
+        String username = usernameField.getText();
+        String nickname = nicknameField.getText();
+        String password = passwordField.getText();
+        if (username.equals("") || nickname.equals("") || password.equals("")) {
+            messageText.setFill(Color.RED);
+            messageText.setText("You must fill in all the fields!");
+        } else {
+            JSONObject controlAnswer = MainView.getInstance().register(username, password, nickname);
+            String messageType = controlAnswer.getString("Type");
+            if (messageType.equals("Successful")) {
+                messageText.setFill(Color.GREEN);
+            } else {
+                messageText.setFill(Color.RED);
+            }
+            String message = controlAnswer.getString("Value");
+            messageText.setText(message);
+            clearFields();
+        }
+    }
+
+    private void clearFields() {
+        usernameField.clear();
+        nicknameField.clear();
+        passwordField.clear();
     }
 }
