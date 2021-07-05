c452dcb58c1f08aa4fa432f46d2c0d446acc455c
MMVahedi <m.m.vahedi13800@gmail.com>
2021-07-05 12:13:58 +0430

    add fx id to the register page field.

--- src/main/java/view/pages/RegisterPage.java
+++ src/main/java/view/pages/RegisterPage.java
@@ -4,11 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class RegisterPage extends Application {
     private static Stage stage;
+    public TextField usernameField;
+    public TextField nicknameField;
+    public PasswordField passwordField;
+    public Text messageText;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent startingPane = FXMLLoader.load(getClass().getResource("/view/fxml/Register.fxml"));
@@ -18,5 +26,8 @@ public class RegisterPage extends Application {
 
     public void back(MouseEvent mouseEvent) throws Exception {
         new StartPage().start(stage);
+    }
+
+    public void login(MouseEvent mouseEvent) {
     }
 }
