68eaf6c5b30f95a0547db6a6371572f04105a730
MMVahedi <m.m.vahedi13800@gmail.com>
2021-07-09 15:15:19 +0430

    fix fx id of DuelMenu parameters.

--- src/main/java/view/pages/DuelMenuPage.java
+++ src/main/java/view/pages/DuelMenuPage.java
@@ -4,15 +4,37 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class DuelMenuPage extends Application {
     private static Stage stage;
+    public TextField numberOfRoundsField;
+    public TextField opponentUsernameField;
+    public Text message;
+    public Button playButton;
+    public Text numberOfRoundsText;
+    public Text opponentUsernameText;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent startingPane = FXMLLoader.load(getClass().getResource("/view/fxml/DuelMenu.fxml"));
         primaryStage.setScene(new Scene(startingPane));
         stage = primaryStage;
+    }
+
+    public void singlePlayMatch(MouseEvent mouseEvent) {
+    }
+
+    public void multiPlayMatch(MouseEvent mouseEvent) {
+    }
+
+    public void back(MouseEvent mouseEvent) {
+    }
+
+    public void play(MouseEvent mouseEvent) {
     }
 }
