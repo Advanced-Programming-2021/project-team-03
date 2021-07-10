6b19e37d665604dcf4e0d034ccccbeeb5607ec1d
Ali Razghandi <a.razghandi11@gmail.com>
2021-07-10 17:25:15 +0430

    add background music

--- src/main/java/view/pages/StartPage.java
+++ src/main/java/view/pages/StartPage.java
@@ -6,7 +6,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class StartPage extends Application {
     private static Stage stage;
@@ -18,9 +22,19 @@ public class StartPage extends Application {
         primaryStage.setScene(scene);
         primaryStage.getIcons().add(new Image(String.valueOf(getClass().getResource("/assets/pageimages/logo.png"))));
         primaryStage.setTitle("YU-GI-OH");
+        backgroundMusic();
         stage = primaryStage;
         primaryStage.setResizable(false);
         primaryStage.show();
+    }
+
+    MediaPlayer mediaPlayer;
+
+    private void backgroundMusic() {
+        mediaPlayer = new MediaPlayer(new Media(Objects.requireNonNull(getClass().getResource("/assets/soundtrack/ThemeSongs.mp3")).toString()));
+        mediaPlayer.setVolume(0.2);
+        mediaPlayer.setCycleCount(-1);
+        mediaPlayer.play();
     }
 
     public void register(MouseEvent mouseEvent) throws Exception {
