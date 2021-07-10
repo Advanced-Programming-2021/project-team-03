161e0332359375d17f86b93463e13589911ff9cf
Alireza Habibzade <alirezahabib80@gmail.com>
2021-07-10 20:24:58 +0430

    Fixing a mistake in duel page

--- src/main/java/view/pages/DeckMenuPage.java
+++ src/main/java/view/pages/DeckMenuPage.java
@@ -5,11 +5,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class DeckMenuPage extends Application {
     private static Stage stage;
+    public ScrollPane scrollPane;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
