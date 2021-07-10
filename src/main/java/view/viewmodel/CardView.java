f59a7b6380237791f1bca56f0e65e4455c937419
MMVahedi <m.m.vahedi13800@gmail.com>
2021-07-10 22:02:37 +0430

    ADD isFull filed in CardView.

--- src/main/java/view/viewmodel/CardView.java
+++ src/main/java/view/viewmodel/CardView.java
@@ -8,24 +8,32 @@ public class CardView extends ImageView {
     Image backImage = new Image(String.valueOf(getClass().getResource("/assets/cards/Unknown.jpg")));
     Image frontImage;
 
+    private boolean isFull;
+    private boolean isFaceUp;
+
     public CardView(int yLocation, int xLocation) {
         this.setLayoutY(yLocation);
         this.setLayoutX(xLocation);
         this.setFitHeight(85);
         this.setFitWidth(75);
+        this.isFaceUp = false;
+        this.isFull = false;
     }
 
     public void putTheCardOnTheBack() {
         this.setImage(backImage);
+        this.isFaceUp = false;
     }
 
     public void putTheCardOnTheFront() {
+        this.isFaceUp = true;
         this.setImage(frontImage);
     }
 
     public void setFrontImage(String cardName) {
         try {
             this.frontImage = MainView.getInstance().getCardImage(cardName);
+            this.isFull = true;
         } catch (Exception exception) {
             this.frontImage = null;
             exception.printStackTrace();
@@ -41,6 +49,12 @@ public class CardView extends ImageView {
     }
 
     public void removeImage() {
+        this.isFull = false;
         this.setImage(null);
     }
+
+    public boolean isFull() {
+        return isFull;
+    }
+
 }
