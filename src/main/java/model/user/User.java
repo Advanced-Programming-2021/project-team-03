e24ea2c08e66d4a3b36ce411f27fb258da7a13d4
Alireza Habibzade <alirezahabib80@gmail.com>
2021-07-10 17:40:56 +0430

    Fixed a bug with user balances Implemented export card with images

--- src/main/java/model/user/User.java
+++ src/main/java/model/user/User.java
@@ -36,7 +36,6 @@ public class User {
         this.username = username;
         this.passwordHash = hashString(password);
         this.score = 0;
-        this.balance = 10000;
         this.level = 1;
 
         decks = new ArrayList<>();
@@ -190,8 +189,10 @@ public class User {
 
         cards.addAll(monsters.stream().limit(30).collect(Collectors.toList()));
         cards.addAll(spellAndTraps.stream().limit(20).collect(Collectors.toList()));
-        updateInDatabase();
+
         profileImageID = (new Random()).nextInt(20) + 1;
+        balance = 10000;
+
         updateInDatabase();
     }
 
--- src/main/java/view/pages/ImportExportPage.java
+++ src/main/java/view/pages/ImportExportPage.java
@@ -1,27 +1,34 @@
 package view.pages;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.input.DragEvent;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import view.viewcontroller.MainView;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
 
 public class ImportExportPage extends Application {
-    private static Stage stage;
-    private static boolean initialized;
+    private final ArrayList<ShopCard> allCards = new ArrayList<>();
+    public ScrollPane scrollPane;
+    private Stage stage;
+    private ShopCard selectedCard;
+    private Pane selectedPane;
 
     @FXML
     public TextField cardNameField;
@@ -34,55 +41,198 @@ public class ImportExportPage extends Application {
         stage = primaryStage;
     }
 
-    public void back(MouseEvent mouseEvent) throws Exception {
-        new MainPage().start(stage);
+    @FXML
+    public void initialize() {
+        loadAllCards();
 
+        GridPane gridPane = new GridPane();
+
+
+        // create a VBox
+        VBox vbox = new VBox();
+
+        try {
+            for (ShopCard card : allCards) {
+                Image image = MainView.getInstance().getCardImage(card.getName());
+                ImageView imageView = new ImageView(image);
+                Pane pane = new Pane(imageView);
+
+                imageView.setOnMouseEntered(mouseEvent -> {
+                    if (selectedCard != card)
+                        pane.setStyle("-fx-border-width: 6;"
+                            + "-fx-border-color: lightgreen;");
+                });
+
+                imageView.setOnMouseExited(mouseEvent -> {
+                    if (selectedCard != card)
+                        pane.setStyle("-fx-border-width: 0;");
+                });
+
+                imageView.setOnMouseClicked(mouseEvent -> {
+                    if (selectedCard == card) {
+                        selectedCard = null;
+                        selectedPane = null;
+                        pane.setStyle("-fx-border-width: 0");
+                    } else {
+                        selectedCard = card;
+                        if (selectedPane != null) selectedPane.setStyle("-fx-border-width: 4;");
+
+                        pane.setStyle("-fx-border-width: 4;"
+                                + "-fx-border-color: lightgreen;");
+                        selectedPane = pane;
+                    }
+                });
+
+                imageView.setPreserveRatio(true);
+                imageView.setFitWidth(350);
+                vbox.getChildren().add(pane);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        scrollPane.setContent(vbox);
+
+
+        importButton.setOnDragEntered(event -> {
+            /* the drag-and-drop gesture entered the target */
+            /* show to the user that it is an actual gesture target */
+            if (event.getGestureSource() != importButton) {
+                if (isJsonFile(event.getDragboard()))
+                    importButton.setStyle("-fx-border-radius: 40;"
+                            + "-fx-border-width: 6;"
+                            + "-fx-border-color: lightgreen;");
+                else
+                    importButton.setStyle("-fx-border-radius: 40;"
+                            + "-fx-border-width: 6;"
+                            + "-fx-border-color: red;");
+            }
+            event.consume();
+        });
+
+        importButton.setOnDragOver(event -> {
+            /* data is dragged over the target */
+            /* accept it only if it is  not dragged from the same node
+             * and if it has a proper file */
+            if (event.getGestureSource() != importButton &&
+                    isJsonFile(event.getDragboard())) {
+                /* allow for both copying and moving, whatever user chooses */
+                event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+            }
+            event.consume();
+        });
+
+        importButton.setOnDragExited(event -> {
+            /* mouse moved away, remove the graphical cues */
+            importButton.setStyle("-fx-border-width: 0;");
+            event.consume();
+        });
+
+        importButton.setOnDragDropped(event -> {
+            /* data dropped */
+            System.out.println("onDragDropped");
+            /* if there is a card file on dragBoard, read it and use it */
+            Dragboard dragboard = event.getDragboard();
+            boolean success = false;
+            if (isJsonFile(dragboard)) {
+                importCardFromFile(dragboard.getFiles().get(0));
+                success = true;
+            }
+            event.setDropCompleted(success);
+            event.consume();
+        });
     }
 
-    public void chooseToImport() {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Choose card file");
-        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Yu-Gi-Oh! card file (*.json)", "*.json");
-        fileChooser.getExtensionFilters().add(extFilter);
-        File file = fileChooser.showOpenDialog(stage);
+    public void back(MouseEvent mouseEvent) throws Exception {
+        new MainPage().start(stage);
+    }
 
-        if (file != null) System.out.println(file.getName());
+    private void loadAllCards() {
+        JSONObject answer = MainView.getInstance().getAllCards();
+        String type = answer.getString("Type");
+        JSONArray value = answer.getJSONArray("Value");
+        if (type.equals("Successful")) {
+            for (int i = 0; i < value.length(); i++) {
+                addCard(value.getString(i));
+            }
+        }
+    }
 
-        //MainView.alertMaker(cardNameField, MainView.getInstance().importExportCard(cardName, "Import card"));
+    private void addCard(String card) {
+        String[] splitInfo = card.split(": ");
+        allCards.add(new ShopCard(splitInfo[1], Integer.parseInt(splitInfo[0])));
+    }
+
+
+    private void importCardFromFile(File file) {
+        if (file == null) return;
+        Alert alert;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String cardFile = reader.readLine();
+            JSONObject answer = MainView.getInstance().importCardJson(cardFile);
+
+            String newCardName = new JSONObject(cardFile).getString("Name");
+
+            if (answer.get("Type").equals("Successful")) {
+                alert = new Alert(Alert.AlertType.INFORMATION);
+            } else {
+                alert = new Alert(Alert.AlertType.ERROR);
+            }
+            alert.setContentText(answer.getString("Value")
+             + "\n" + MainView.getInstance().showCard(newCardName).getString("Value"));
+        } catch (Exception e) {
+            alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Couldn't read card file: " + e.getMessage());
+        }
+        alert.show();
     }
 
     public void importCard(MouseEvent mouseEvent) {
-        if (initialized) chooseToImport();
-
-        importButton.setText("Drag and drop to import \n" +
-                "or click here to choose manually");
-
-        importButton.setOnDragEntered(new EventHandler<DragEvent>() {
-            public void handle(DragEvent event) {
-                /* the drag-and-drop gesture entered the target */
-                System.out.println("onDragEntered");
-                /* show to the user that it is an actual gesture target */
-                if (event.getGestureSource() != importButton) {
-                    if (isJsonFile(event.getDragboard()))
-                        importButton.setStyle("-fx-border-radius: 40;"
-                                + "-fx-border-width: 5;"
-                                + "-fx-border-color: lightgreen;");
-                    else
-                        importButton.setStyle("-fx-border-radius: 40;"
-                                + "-fx-border-width: 5;"
-                                + "-fx-border-color: red;");
-                }
-                event.consume();
-            }
-        });
-
-        initialized = true;
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose card file");
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Yu-Gi-Oh card (*.json)", "*.json");
+        fileChooser.getExtensionFilters().add(extFilter);
+        File file = fileChooser.showOpenDialog(stage);
+        importCardFromFile(file);
     }
 
     public void exportCard(MouseEvent mouseEvent) {
+
+
         String cardName = cardNameField.getText();
+        MainView view = MainView.getInstance();
         if (cardName.equals("")) return;
-        MainView.getInstance().alertMaker(cardNameField, MainView.getInstance().importExportCard(cardName, "Export card"));
+        JSONObject answer = view.getCardJson(cardName);
+
+        if (!answer.get("Type").equals("Successful")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText(answer.getString("Value"));
+            alert.show();
+            return;
+        }
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select a directory to save the card file");
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Yu-Gi-Oh card (*.json)", "*.json");
+        fileChooser.getExtensionFilters().add(extFilter);
+        fileChooser.setInitialFileName(cardName);
+        File file = fileChooser.showSaveDialog(stage);
+        if (file == null) return;
+
+        Alert alert;
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(answer.getString("Value"));
+            fileWriter.close();
+
+            alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setContentText("Card save successfully at: " + file.getAbsolutePath());
+        } catch (IOException e) {
+            alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Couldn't export card: " + e.getMessage());
+        }
+        alert.show();
     }
 
     private boolean isJsonFile(Dragboard dragboard) {
